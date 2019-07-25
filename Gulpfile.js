94f0ae04f79025486b4dda6243b96138937d93eb
Javier Preda <javier.preda@gmail.com>
2019-07-25 11:02:17 -0400

    ajustando gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var file = require('gulp-file');
+const shell = require('gulp-shell');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
@@ -9,6 +11,23 @@ gulp.task('frontstart', function() {
   .pipe(gulp.dest('./app/'));
 
   //then we will copy bootstrap scss core file to our own scss folder
-
+  gulp.start('copybootstrap');
 
 });
+
+gulp.task('copybootstrap', function() {
+
+  //Copy the html5-boilerplate dist to app/ folder
+  gulp.src('./node_modules/bootstrap/scss/**/*.*')
+  .pipe(gulp.dest('./app/scss/bootstrap'));
+
+  gulp.start('createfile');
+  //then we will copy bootstrap scss core file to our own scss folder
+
+});
+
+gulp.task('createfile', () => {
+  return gulp
+    .src('./app/scss/', { read: false })
+    .pipe(shell(['touch main.scss']));
+})
