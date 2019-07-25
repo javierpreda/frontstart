cf8392bbeffe8069c8614b1a7c81b23affc7bdbf
Javier Preda <javier.preda@gmail.com>
2019-07-25 16:19:16 -0400

    gulpfile working as expected, adding readme file

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,33 +1,59 @@
 var gulp = require('gulp');
-var file = require('gulp-file');
-const shell = require('gulp-shell');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+var connect = require('gulp-connect');
 
-gulp.task('frontstart', function() {
+var exec = require('child_process').exec;
+
+gulp.task('init', function() {
 
   //Copy the html5-boilerplate dist to app/ folder
+  console.log("Copying html5 boilerplate to our app/ folder...");
   gulp.src('./node_modules/html5-boilerplate/dist/**/*.*')
   .pipe(gulp.dest('./app/'));
 
   //then we will copy bootstrap scss core file to our own scss folder
-  gulp.start('copybootstrap');
+  console.log("Copying bootstrap core scss to our app/scss/bootstrap folder...");
+  return gulp.src('./node_modules/bootstrap/scss/**/*.*').pipe(gulp.dest('./app/scss/bootstrap'));
 
 });
 
-gulp.task('copybootstrap', function() {
-
-  //Copy the html5-boilerplate dist to app/ folder
-  gulp.src('./node_modules/bootstrap/scss/**/*.*')
-  .pipe(gulp.dest('./app/scss/bootstrap'));
-
-  gulp.start('createfile');
-  //then we will copy bootstrap scss core file to our own scss folder
-
+gulp.task('createmain', function (cb) {
+  console.log("Creating main.scss file as starting point of frontend developing...");
+  return exec('touch app/scss/main.scss', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
 });
 
-gulp.task('createfile', () => {
-  return gulp
-    .src('./app/scss/', { read: false })
-    .pipe(shell(['touch main.scss']));
-})
+// keeps gulp from crashing for scss errors
+gulp.task('sass', function () {
+   return gulp.src('app/scss/*.scss')
+      .pipe(sass({ errLogToConsole: true }))
+      .pipe(gulp.dest('app/css/'));
+});
+
+gulp.task('livereload', function (){
+  return gulp.src('app/**/*')
+  .pipe(connect.reload());
+});
+
+gulp.task('watch', function (cb) {
+  gulp.watch('app/scss/**/*.scss').on('change', gulp.series('sass', 'livereload'));
+  gulp.watch('app/**/*').on('change', gulp.series('livereload'));
+  console.log("ejecutando watch...");
+  cb();
+});
+
+gulp.task('connect', function(){
+   connect.server({
+    root: 'app',
+    livereload: true
+  });
+});
+
+gulp.task('frontstart', gulp.series('init', 'createmain'));
+
+gulp.task('happyfrontending', gulp.parallel('connect', 'watch'));
+
+
---
+++ README.md
@@ -0,0 +1,38 @@
+
+# Frontstart - Quickly make awesome projects with the help of html5-boilerplate and Bootstrap 
+
+Frontstart is a series of gulp tasks that help you quickly setup your frontend development, without too many complex packages or libraries, is like an artist blank canvas. The tasks basically are the following:
+
+- Copy html5-boilerplate dist folder from node_modules to your our main app folder
+- Copy bootstrap core scss files to our core app/scss folder
+- Create an empty main.scss file for you to start your journey from there. You should import bootstrap files and your custom scss here, is up to you how you want to proceed.
+
+# Setup
+
+Fist, check if you have node and gulp installed, then:
+
+```
+npm install
+```
+
+then:
+
+```
+gulp frontstart
+```
+
+and lastly:
+
+```
+gulp happyfrontending
+```
+and Voila!
+
+# Dependencies
+
+- gulp
+- gulp-sass
+- gulp-connect
+- html5-boilerplate
+- bootstrap
+
